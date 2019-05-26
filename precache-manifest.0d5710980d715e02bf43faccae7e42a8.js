bfbdc8f7e60873fcfaef68976a1ae7c8d7605091
John Redding <john.redding@live.com>
2019-05-26 18:26:11 -0400

    Updates

---
+++ precache-manifest.0d5710980d715e02bf43faccae7e42a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c87d08993e30f071316105d819be5c",
+    "url": "/RecipeBook/index.html"
+  },
+  {
+    "revision": "3d72b225710e7054a69b",
+    "url": "/RecipeBook/static/css/main.fe14913a.chunk.css"
+  },
+  {
+    "revision": "4dc77dc907998a66d9c8",
+    "url": "/RecipeBook/static/js/2.7440b25f.chunk.js"
+  },
+  {
+    "revision": "3d72b225710e7054a69b",
+    "url": "/RecipeBook/static/js/main.b4e94ecd.chunk.js"
+  },
+  {
+    "revision": "465103f1f39e4ff50632",
+    "url": "/RecipeBook/static/js/runtime~main.3c89a3ed.js"
+  }
+]);
